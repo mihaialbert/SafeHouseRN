bef10f20d8b57a51734f8f515870f45b66c6fef3
mihaialbert <mihai.albert.ioan@gmail.com>
2019-12-14 18:08:38 +0200

    More Charts

--- screens/ChartsScreen.js
+++ screens/ChartsScreen.js
@@ -1,55 +1,81 @@
 import React from 'react';
-import { ScrollView, StyleSheet, View, Text, Dimensions } from 'react-native';
+import { StyleSheet, View, Dimensions } from 'react-native';
 import {
   LineChart
 } from "react-native-chart-kit";
+import { AuthSession } from 'expo';
+
+const chartDataSets = [{
+  labels: ["January", "February", "March", "April", "May", "June"],
+  datasets: [
+    {
+      data: [
+        Math.random() * 100,
+        Math.random() * 100,
+        Math.random() * 100,
+        Math.random() * 100,
+        Math.random() * 100,
+        Math.random() * 100
+      ]
+    }
+  ]
+},
+{
+  labels: ["16:00", "17:00", "18:00", "19:00", "20:00", "21:00"],
+  datasets: [
+    {
+      data: [
+        Math.random() * 100,
+        Math.random() * 100,
+        Math.random() * 100,
+        Math.random() * 100,
+        Math.random() * 100,
+        Math.random() * 100
+      ]
+    }
+  ]
+}
+]
 
 export default function ChartsScreen() {
   return (
     <View>
-      <Text>Bezier Line Chart</Text>
-      <LineChart
-        data={{
-          labels: ["January", "February", "March", "April", "May", "June"],
-          datasets: [
-            {
-              data: [
-                Math.random() * 100,
-                Math.random() * 100,
-                Math.random() * 100,
-                Math.random() * 100,
-                Math.random() * 100,
-                Math.random() * 100
-              ]
-            }
-          ]
-        }}
-        width={Dimensions.get("window").width} // from react-native
-        height={220}
-        yAxisLabel={"$"}
-        yAxisSuffix={"k"}
-        chartConfig={{
-          backgroundColor: "#e26a00",
-          backgroundGradientFrom: "#fb8c00",
-          backgroundGradientTo: "#ffa726",
-          decimalPlaces: 2, // optional, defaults to 2dp
-          color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
-          labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
-          style: {
-            borderRadius: 16
-          },
-          propsForDots: {
-            r: "6",
-            strokeWidth: "2",
-            stroke: "#ffa726"
-          }
-        }}
-        bezier
-        style={{
-          marginVertical: 8,
-          borderRadius: 16
-        }}
-      />
+      {
+        chartDataSets.map((dataSet, index) => {
+          return (
+            <LineChart
+              key={index}
+              data={dataSet}
+              width={Dimensions.get("window").width} // from react-native
+              height={220}
+              yAxisLabel={"$"}
+              yAxisSuffix={"k"}
+              chartConfig={{
+                backgroundColor: "#e26a00",
+                backgroundGradientFrom: "#fb8c00",
+                backgroundGradientTo: "#ffa726",
+                decimalPlaces: 2, // optional, defaults to 2dp
+                color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+                labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+                style: {
+                  borderRadius: 16
+                },
+                propsForDots: {
+                  r: "6",
+                  strokeWidth: "2",
+                  stroke: "#ffa726"
+                }
+              }}
+              bezier
+              style={{
+                marginVertical: 8,
+                borderRadius: 16
+              }}
+            />
+          )
+        })
+      }
+
     </View>
   );
 }
@@ -63,5 +89,5 @@ const styles = StyleSheet.create({
     flex: 1,
     paddingTop: 15,
     backgroundColor: '#fff',
-  },
+  }
 });
